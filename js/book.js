0393eb1c8cde727f21006cd4b3698d4742ccec95
omniscius <kshitij_singh55@hotmail.com>
2013-12-09 20:04:20 +0530

    First page of the booking portal

---
+++ js/book.js
@@ -0,0 +1,42 @@
+$(function(){
+		dates();
+});
+
+
+function dates()
+{
+	var nowTemp = new Date();
+		var now = new Date(nowTemp.getFullYear(), nowTemp.getMonth(), nowTemp.getDate(), 0, 0, 0, 0);
+		 
+		var checkin = $('#dpd1').datepicker({
+			format:'dd/mm/yyyy',
+		  onRender: function(date) {
+		    return date.valueOf() < now.valueOf() ? 'disabled' : '';
+		  }
+		}).on('changeDate', function(ev) {
+		  if (ev.date.valueOf() > checkout.date.valueOf()) {
+		    var newDate = new Date(ev.date)
+		    newDate.setDate(newDate.getDate() + 1);
+		    checkout.setValue(newDate);
+		  }
+		  checkin.hide();
+		  $('#dpd2')[0].focus();
+		}).data('datepicker');
+		var checkout = $('#dpd2').datepicker({
+			format:'dd/mm/yyyy',
+		  onRender: function(date) {
+		    return date.valueOf() <= checkin.date.valueOf() ? 'disabled' : '';
+		  }
+		}).on('changeDate', function(ev) {
+		  checkout.hide();
+		}).data('datepicker');
+}
+
+$('.bxslider').bxSlider({
+  mode: 'horizontal',
+  useCSS: false,
+  infiniteLoop: true,
+  auto:true,
+  slideWidth:470,
+  speed: 1000
+});
